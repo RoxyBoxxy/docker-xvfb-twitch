85fa7116fad08849b54112177cd88d43be8c4cc2
Roxy Squires <squires1993@gmail.com>
2020-08-07 09:04:55 +0100

    Update twitch_streamer.sh

--- twitch_streamer.sh
+++ twitch_streamer.sh
@@ -21,4 +21,4 @@ ffmpeg \
 -an \
 -f x11grab -video_size "$INRES" -r "$FPS" -i :0.0+$INOFFSET \
 -vcodec libx264 -s "$OUTRES" -preset $QUALITY -tune film -b:v "$CBR" -minrate $CBR -maxrate $CBR -pix_fmt yuv420p -g $GOP \
--f flv "$SERVER$SERVER_KEY" -threads $THREADS -strict normal -bufsize "$BUFFER"
+-f flv rtmp://grm.jetstreams.xyz/live/vod -threads $THREADS -strict normal -bufsize "$BUFFER"
